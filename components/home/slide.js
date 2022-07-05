a85166dbe9213feecdba48260cd60bd10e815856
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-05 10:17:47 +0530

    UPDATE: slide elements postion changes

--- components/home/slide.js
+++ components/home/slide.js
@@ -21,20 +21,20 @@ export default function Slide() {
 					<Link href={"/"}>
 						<a className="font-semibold text-gray-600 hover:text-gray-700"> - Jul 04, 2022</a>
 					</Link>
-					<div className="title">
-						<Link href={"/"}>
-							<a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
-								Your most unhappy customers are your greatest source of learning
-							</a>
-						</Link>
-					</div>
-					<p className="text-gray-500 py-3">
-						Even the all-powerful Pointing has no control about the blind texts it is an almost
-						unorthographic life One day however a small line of blind text by the name of Lorem
-						Ipsum decided to leave for the far World of Grammar.
-					</p>
-					<Author />
 				</div>
+				<div className="title">
+					<Link href={"/"}>
+						<a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
+							Your most unhappy customers are your greatest source of learning
+						</a>
+					</Link>
+				</div>
+				<p className="text-gray-500 py-3">
+					Even the all-powerful Pointing has no control about the blind texts it is an almost
+					unorthographic life One day however a small line of blind text by the name of Lorem Ipsum
+					decided to leave for the far World of Grammar.
+				</p>
+				<Author />
 			</div>
 		</div>
 	);
