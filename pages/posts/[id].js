52a3dba2b85cda3e85a097922b1f34e4a1d4f0ff
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-06 15:28:53 +0530

    UPDATE: layout wrapper added to error, spinner

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -16,37 +16,45 @@ export default function PostPage() {
 
 	const { data, isLoading, isError } = useFetcher(`posts/${id}`);
 
-	if (isLoading) return <Spinner />;
-	if (isError) return <Error message={`While fetching post ${id}.`} />;
-
-	if (data) {
-		const { title, description, category, published, img, author } = data;
-
+	if (isLoading)
 		return (
 			<Layout>
-				<div className="container mx-auto py-16 px-8 lg:w-1/2 md:w-2/3 w-full">
-					<div className="flex justify-center">{author && <Author author={author} />}</div>
-					<div className="post py-10">
-						<h1 className="text-3xl md:text-5xl font-bold text-gray-800 pb-8 text-center">
-							{title || "Unknown"}
-						</h1>
-						<div className="pb-5">
-							<Image
-								src={img || "/images/img1.jpg"}
-								width={900}
-								height={600}
-								alt={"trending-blog-image"}
-								className="object-cover"
-							/>
-						</div>
-						{category && published ? <PostInfo info={{ category, published }} /> : ""}
-						<div className="text-gray-600 py-3 text-justify text-lg flex flex-col gap-4 pt-4">
-							<p>{description || "Unknown"}</p>
-						</div>
-					</div>
-					<RelatedSection />
-				</div>
+				<Spinner />
 			</Layout>
 		);
-	}
+	if (isError)
+		return (
+			<Layout>
+				<Error message={`While fetching post ${id}.`} />
+			</Layout>
+		);
+
+	const { title, description, category, published, img, author } = data;
+
+	return (
+		<Layout>
+			<div className="container mx-auto py-16 px-8 lg:w-1/2 md:w-2/3 w-full">
+				<div className="flex justify-center">{author && <Author author={author} />}</div>
+				<div className="post py-10">
+					<h1 className="text-3xl md:text-5xl font-bold text-gray-800 pb-8 text-center">
+						{title || "Unknown"}
+					</h1>
+					<div className="pb-5">
+						<Image
+							src={img || "/images/img1.jpg"}
+							width={900}
+							height={600}
+							alt={"trending-blog-image"}
+							className="object-cover"
+						/>
+					</div>
+					{category && published ? <PostInfo info={{ category, published }} /> : ""}
+					<div className="text-gray-600 py-3 text-justify text-lg flex flex-col gap-4 pt-4">
+						<p>{description || "Unknown"}</p>
+					</div>
+				</div>
+				<RelatedSection />
+			</div>
+		</Layout>
+	);
 }
