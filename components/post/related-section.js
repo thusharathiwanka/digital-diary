f2e75a9c999d718bcb0651438da2e80240a675d0
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-06 15:26:39 +0530

    CREATE: related post section connected

--- components/post/related-section.js
+++ components/post/related-section.js
@@ -1,18 +1,24 @@
 import CategoryBlogCard from "../home/category-blog-card";
+import Error from "../common/children/error";
+import Spinner from "../common/children/spinner";
+
+import useFetcher from "../../hooks/useFetcher";
 
 export default function RelatedSection() {
+	const { data, isLoading, isError } = useFetcher("posts");
+
+	if (isLoading) return <Spinner />;
+	if (isError) return <Error message={"While fetching categorized posts."} />;
+
 	return (
 		<section className="pt-20">
 			<h1 className="font-bold text-3xl py-10">Related Posts</h1>
-			<div className="pb-5">
-				<CategoryBlogCard />
-			</div>
-			<div className="pb-5">
-				<CategoryBlogCard />
-			</div>
-			<div className="pb-5">
-				<CategoryBlogCard />
-			</div>
+			{data.length > 0 &&
+				data.slice(0, 3).map(post => (
+					<div className="pb-5" key={post.id}>
+						<CategoryBlogCard post={post} />
+					</div>
+				))}
 		</section>
 	);
 }
